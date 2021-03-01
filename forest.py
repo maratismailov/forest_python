c6777c7cc28d7a39fbf8715501d75bc3f5a43102
Marat Ismailov <regpo55555@gmail.com>
2021-03-01 13:39:09 +0600

    Update forest.py and insertStandEstimation.py

--- forest.py
+++ forest.py
@@ -8,6 +8,7 @@ import json
 import sys
 import psycopg2
 from stand_estimation_oopt.insertStandEstimation import insert_stand_estimation_oopt
+from stand_estimation_leshoz.insertStandEstimation import insert_stand_estimation_leshoz
 # from stand_estimation_oopt.fore
 # from control_table import path, lezhoz_path, oopt_path, inv_path, geodata_rent_path, path_to_remove, path_to_copy
 
@@ -36,6 +37,21 @@ for filename in os.listdir('incoming'):
                     json.dump(newDict, outfile, indent=4)
                 insert_stand_estimation_oopt(newDict)
                 jsonNumber = jsonNumber+1
+    elif 'leshoz' in filename:
+        newFilename = filename
+        zf = zipfile.ZipFile(incoming + filename, 'r')
+        jsonNumber = 1
+        for name in zf.namelist():
+            if name.endswith('/'):
+                continue
+            if 'data/' in name:
+                f = zf.open(name).read()
+                newDict = xmltodict.parse(f, force_list={'forest_composition','forest_use_type','planned_forest_use_type','action_priority_1', 'tillage_1', 'creation_type_1', 'action_priority_2', 'action_priority_2', 'tillage_2', 'creation_type_2', 'planned_forest_composition'})
+                with open(filename+str(jsonNumber)+'.json', 'w') as outfile:
+                    json.dump(newDict, outfile, indent=4)
+                insert_stand_estimation(newDict)
+                jsonNumber = jsonNumber+1
+    os.remove(filename)
 # for filename in glob.glob(path):
 #     print(path)
 #     if 'rent_data' in filename:
